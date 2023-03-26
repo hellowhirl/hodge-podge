b052d666db4207ed2c9ae3c313ffb6887fd0826a
Anthony Avila <hellowhirl@users.noreply.github.com>
2023-03-26 16:14:31 +0900

    revise sumTimes function, takes array of strings

--- addUpTimes.js
+++ addUpTimes.js
@@ -1,29 +1,34 @@
-const testing = ["3:29", "5:51", "6:14"];
-const minutes = [3, 5, 6, 3, 5, 2, 4, 2, 4, 7, 3, 4, 3, 2, 4, 3, 4];
-const seconds = [
-  29, 51, 14, 54, 32, 22, 34, 22, 53, 22, 50, 48, 55, 03, 49, 25, 33,
-];
+function sumTimes(times) {
+  // Initialize variables to hold the total minutes and seconds
+  let totalMinutes = 0;
+  let totalSeconds = 0;
 
-let addUpTimes = (times) => {
-  debugger;
-  let minutes = 0;
-  let seconds = 0;
-
+  // Loop through each time in the array
   for (let i = 0; i < times.length; i++) {
-    minutes += Number(times[i][times[i].length - 5] * 10);
-    minutes += Number(times[i][times[i].length - 4] * 10);
+    // Split the time into minutes and seconds
+    const [minutes, seconds] = times[i].split(':').map(Number);
+
+    // Add the minutes and seconds to the total variables
+    totalMinutes += minutes;
+    totalSeconds += seconds;
   }
 
-  for (let i = 0; i < times.length; i++) {
-    seconds += Number(times[i][times[i].length - 2] * 10);
-    seconds += Number(times[i][times[i].length - 1]);
-  }
+  // Convert excess seconds to minutes
+  totalMinutes += Math.floor(totalSeconds / 60);
+  totalSeconds = totalSeconds % 60;
 
-  let convertedSeconds = Math.floor(seconds / 60);
-  seconds = seconds % 60;
-  minutes += convertedSeconds;
+  // Format the total time as "mm:ss"
+  const formattedMinutes = totalMinutes.toString();
+  const formattedSeconds = totalSeconds.toString().padStart(2, '0');
 
-  console.log(`${minutes}:${seconds}`);
-};
+  return `${formattedMinutes}:${formattedSeconds}`;
+}
 
-addUpTimes(testing);
+// Example usage
+const times = ['1:09', '2:45', '0:45'];
+const totalTime = sumTimes(times);
+console.log(totalTime); // Output: "4:39"
+
+const myTimes = ['0:01', '0:02']
+
+sumTimes(myTimes)
